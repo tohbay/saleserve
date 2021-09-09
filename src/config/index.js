b795f4e684cc823bd44c514d2b533b665ce77a9f
tohbay <tobechukwuobitube@gmail.com>
2021-09-09 14:45:15 +0100

    fix broken database connection on heroku

--- src/config/index.js
+++ src/config/index.js
@@ -7,6 +7,15 @@ const config = {
     dialect: "postgres",
     ssl: true,
     logging: true,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    ssl: true,
+    logging: true,
   }
 };
 
